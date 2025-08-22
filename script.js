e617b603799db80df9c097374d10450ed433f6a4
p88083138-dev <p88083138@gmail.com>
2025-08-22 14:17:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// Test button
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("clickMeBtn").addEventListener("click", function () {
-        alert("Hello from MyStudySpire!");
-    });
-});
-
-// Notes download function
-function downloadNotes(subject) {
-    if (subject === "Maths") {
-        window.open("https://www.africau.edu/images/default/sample.pdf", "_blank");
-    } else if (subject === "Science") {
-        window.open("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf", "_blank");
-    } else if (subject === "English") {
-        window.open("https://file-examples.com/storage/fe7f66a3e5d04e7b8bdf5fc/2017/10/file-sample_150kB.pdf", "_blank");
-    }
+function downloadNotes() {
+    alert("Your notes are downloading...");
+    // Yeh line repo ke andar wali file open karegi
+    window.open("notes.pdf", "_blank");
 }
+
+document.getElementById("clickMeBtn").addEventListener("click", downloadNotes);
 
