219b618cad1cabcc47f4902dd93f10fe2df9ac6c
p88083138-dev <p88083138@gmail.com>
2025-08-22 13:34:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function downloadNotes(subject) {
+    alert(subject + " notes downloading...");
+
+    // Yaha apna PDF file link dalna hoga
+    if(subject === "Maths") {
+        window.open("https://example.com/maths.pdf", "_blank");
+    } else if(subject === "Science") {
+        window.open("https://example.com/science.pdf", "_blank");
+    } else if(subject === "English") {
+        window.open("https://example.com/english.pdf", "_blank");
+    }
+}
